722e814fef1254aff4e4f4883d267fca49166b9e
Aruzhan Ismagulova <aruchanism@gmail.com>
2025-05-15 01:06:55 +0500

    add new library: angle sharp

--- ContextProviderApp/Services/DictionaryService.cs
+++ ContextProviderApp/Services/DictionaryService.cs
@@ -1,5 +1,7 @@
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 using ContextProviderApp.Models;
-using HtmlAgilityPack;
 using Newtonsoft.Json;
 
 namespace ContextProviderApp.Services
@@ -14,27 +16,33 @@ namespace ContextProviderApp.Services
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<string> GetDictionaryAsync(string text)
+        public async Task<string?> GetDictionaryAsync(string text)
         {
-            var client = _httpClientFactory.CreateClient("Dictionary");
-           // HttpResponseMessage response = client.GetAsync($"api/v2/entries/en/{text}").ConfigureAwait(false).GetAwaiter().GetResult();
-           // HttpResponseMessage response = await client.GetAsync($"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={_apiKey}&lang=en-en&text=time");
-            string response = await client.GetStringAsync($"https://dictionary.cambridge.org/us/dictionary/english/flabbergast");
-            HtmlDocument htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(response);
+            try
+            {
+                AngleSharp.IConfiguration configuration = Configuration.Default.WithDefaultLoader();
+                string adress = $"https://dictionary.cambridge.org/us/dictionary/english/{text}";
+                IBrowsingContext context = BrowsingContext.New(configuration);
+                IDocument doc = await context.OpenAsync(new Url(adress));
+                string a = "div.di-title > span";
+                var cell = doc.QuerySelector<IHtmlElement>(a);
+                var b = cell?.Children.FirstOrDefault()?.InnerHtml;
 
-            return htmlDocument.DocumentNode.InnerText;
-            // if (response.IsSuccessStatusCode)
-            // {
-            //     var contentJson = await response.Content.ReadAsStringAsync();
+                //     var client = _httpClientFactory.CreateClient("Dictionary");
 
-            //     var trimmedJson = contentJson.TrimStart('[').TrimEnd(']');
+                //    // HttpResponseMessage response = client.GetAsync($"api/v2/entries/en/{text}").ConfigureAwait(false).GetAwaiter().GetResult();
+                //    // HttpResponseMessage response = await client.GetAsync($"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={_apiKey}&lang=en-en&text=time");
+                //     string response = await client.GetStringAsync($"https://dictionary.cambridge.org/us/dictionary/english/flabbergast");
+                //     HtmlDocument htmlDocument = new HtmlDocument();
+                //     htmlDocument.LoadHtml(response);
 
-            //     var result = JsonConvert.DeserializeObject<Result>(trimmedJson);
-            //     return result;
-            // }
-
-            throw new Exception("couldn't fetch data");
+                return b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new Exception("couldnt fetch data");
+            }
         }
     }
 
