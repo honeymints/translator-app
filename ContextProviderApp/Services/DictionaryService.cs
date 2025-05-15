5e86f0ea098da4fd6360b2618d67f0967134cbfd
Aruzhan Ismagulova <aismagulova@devexpert.kz>
2025-05-15 19:14:03 +0500

    Refactor GetDictionaryAsync method to enhance HTML element selection and parsing logic

--- ContextProviderApp/Services/DictionaryService.cs
+++ ContextProviderApp/Services/DictionaryService.cs
@@ -24,17 +24,22 @@ namespace ContextProviderApp.Services
                 string adress = $"https://dictionary.cambridge.org/us/dictionary/english/{text}";
                 IBrowsingContext context = BrowsingContext.New(configuration);
                 IDocument doc = await context.OpenAsync(new Url(adress));
-                string a = "div.di-title > span";
-                var cell = doc.QuerySelector<IHtmlElement>(a);
+                string title = "div.di-title > span";
+                string mainbodyEl = "div.pr.dictionary";
+                var cell = doc.QuerySelector<IHtmlElement>(title);
+                var mainBodyElement = doc.QuerySelectorAll<IHtmlElement>(mainbodyEl).FirstOrDefault(x => x.GetAttribute("data-id").Equals("cald4-us"));
+                var mainSectionElement = mainBodyElement?.Children.FirstOrDefault(x => x.ClassName.Equals("link"));
+                var listOfSectionNumber = mainSectionElement?.Children?.FirstOrDefault(x => x.ClassName.Equals("pr di superentry"))?.Children?.Count(x => x.ClassName.Equals("di-body"));
+
+                string a = "div.pos-header.dpos-h";
+                var headers = doc.QuerySelector<IHtmlElement>(a);
+                for (int i = 0; i < listOfSectionNumber; i++)
+                {
+
+
+                }
+
                 var b = cell?.Children.FirstOrDefault()?.InnerHtml;
-
-                //     var client = _httpClientFactory.CreateClient("Dictionary");
-
-                //    // HttpResponseMessage response = client.GetAsync($"api/v2/entries/en/{text}").ConfigureAwait(false).GetAwaiter().GetResult();
-                //    // HttpResponseMessage response = await client.GetAsync($"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={_apiKey}&lang=en-en&text=time");
-                //     string response = await client.GetStringAsync($"https://dictionary.cambridge.org/us/dictionary/english/flabbergast");
-                //     HtmlDocument htmlDocument = new HtmlDocument();
-                //     htmlDocument.LoadHtml(response);
 
                 return b;
             }
